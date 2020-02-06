320ad03e6664e7d7163b400051d5d8fb046b4f1d
Heberth Junor Teixeira Aguiar <hjunor@gmail.com>
2020-02-06 10:54:22 -0300

    organizando

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const Resposta = require('./database/resposta')
 
 connection.authenticate().then(()=>{
   console.log('tudo certo')
-  
 }).catch((erro)=>{
   console.log('erro');
 })
@@ -25,64 +24,63 @@ app.use(bodyParser.json());
 
 //Routes
 app.get('/',(req,res)=>{
-  Pergunta.findAll({raw:true, order:[
-    ['createdAt','DESC']]})// ASC = Crescente || DESC = decresente
-    .then((pergunta)=>{
+  Pergunta.findAll({
+    raw:true, 
+    order:[['createdAt','DESC']]// ASC = Crescente || DESC = decresente
+  }).then((pergunta)=>{
     res.render('index',{
       perguntas:pergunta
-    })  
-})
-});
+    })});
+  });
+
 app.get('/question', (req, res) => {
  res.render('perguntar');
 })
+
 app.get('/pergunta/:id',(req, res)=>{
    var id = req.params.id;
-   Pergunta.findOne({
-     raw:true,
-     where:{ id:id }
-   }).then((pergunta)=>{
+    Pergunta.findOne({
+      raw:true,
+      where:{ id:id }
+    }).then((pergunta)=>{
       if(pergunta!=undefined){
-        Resposta.findAll({where:{
-          perguntaId:pergunta.id
-        }, order:[
-          ['createdAt','DESC']]
-      }).then(respostas =>{
+        Resposta.findAll({
+          where:{ perguntaId:pergunta.id },
+           order:[['createdAt','DESC']]
+        }).then(respostas =>{
           res.render('pergunta',{
            pergunta:pergunta,
            respostas:respostas
-         })
         })
+    })
       }else{
-         res.redirect('/');
+        res.redirect('/');
       }
    })
 })
+
 app.post('/savequestion', (req, res)=>{
     var titulo = req.body.titulos
     var descricao = req.body.descricao
-  Pergunta.create({
-    titulo: titulo,
-    description:descricao
-  }).then(()=>{
-    res.redirect('/');
-  })
+    Pergunta.create({
+      titulo: titulo,
+      description:descricao
+      }).then(()=>{
+        res.redirect('/');
+      })
 })
 
 app.post('/responder', (req,res) => {
   var corpo = req.body.corpo
   var perguntaId = req.body.pergunta
-  console.log(perguntaId);
   Resposta.create({
     corpo:corpo,
     perguntaId:perguntaId,
   }).then(()=>{
-    res.redirect('/pergunta/'+perguntaId)
-    console.log(perguntaId);
+    res.redirect('/pergunta/'+perguntaId)   
   })
  
 });
 
 app.listen(port, () => console.log(`server start`))
 
-//aula importações de variaveis...
